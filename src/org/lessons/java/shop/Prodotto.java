1040f9e7f67b485badabcce577b32e954c414c46
nmorrone <n.morrone96@gmail.com>
2024-09-30 18:57:12 +0200

    product-class

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,12 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
 
-}
+//definisco la classe con le sue caratteristiche
+public class Prodotto {
+			String code;
+			String name;
+			String description;
+			float price;
+			float iva = price + (price * 100 / 22);
+	}
+
