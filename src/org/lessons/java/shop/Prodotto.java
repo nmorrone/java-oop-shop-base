6b81cfa86f46e8229d381c3db1f1e98ffbc075e2
nmorrone <n.morrone96@gmail.com>
2024-10-02 14:00:27 +0200

    fullprice-iva-code-methods

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -7,15 +7,34 @@ public class Prodotto {
 			int code;
 			String name;
 			String description;
-			float price;
-			float iva = price + (price * 100 / 22);
+			double price;
+			double iva = (price * 22) / 100;
+			double fullPrice;
 
 Random generatore = new Random();
 
+//creo metodo per assegnare un codice random di tot cifre ad un prodotto
 int generatoreCodice() {
 	int uniqueCode = generatore.nextInt(1+ 100000);
 	code = uniqueCode;
 	return code;
+}
+
+//definisco il metodo per calcolare l'iva
+//restituirà nel prodotto la variabile calcolata
+double ivaProdotto(double price) {
+	
+	iva = (price * 22) / 100;
+	return iva;
+	
+}
+
+//definisco il metodo per calcolare l'intero prezzo del protto
+//lo salvo in una variabile
+double priceCalculator (double price, double iva) {
+	
+	fullPrice = price + iva;
+	return fullPrice;
 }
 
 }
--- src/org/lessons/java/shop/ProdottoMetodi.java
+++ src/org/lessons/java/shop/ProdottoMetodi.java
@@ -3,19 +3,29 @@ package org.lessons.java.shop;
 
 public class ProdottoMetodi {
 
-	
+//creo l'istanza di un prodotto casuale	
 public static void main(String [] args) {
 	
 	Prodotto newProdotto = new Prodotto();
 	
-	newProdotto.code= 0;
+	newProdotto.name = "Airpod Pro 3th gen";
+	newProdotto.description = "Wireless professional in-ear speakers for iphone and ios devices, 3th generation with the last and modern utilities";
+	newProdotto.price = 299.00;
 	
-	System.out.println(newProdotto.code);
-	
+	//metodo codice prodotto
 	newProdotto.generatoreCodice();
-	
+	//verifico funzionamento
 	System.out.println(newProdotto.code);
 	
+	//metodo iva prodotto
+	newProdotto.ivaProdotto(newProdotto.price);
+	//verifico funzionamento
+	System.out.println(newProdotto.iva);
+	
+	//metodo prezzo completo
+	newProdotto.priceCalculator(newProdotto.price, newProdotto.iva);
+	//verifico funzionamento
+	System.out.println(newProdotto.fullPrice);
 }
 
 
